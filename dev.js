0802930e0c4c341c2024ad03a0196c2658990235
Dan Behlings <nevenall@hotmail.com>
2019-01-31 13:28:29 -0800

    setup sub_sup example to walk through

--- dev.js
+++ dev.js
@@ -1,4 +1,3 @@
-
 // there are four possible special terms
 // /term/ one_slash_term
 // //term// two_slash_term 
@@ -12,7 +11,13 @@
 // what about /{term}/ ? that should wrap the text {term} in the 
 // markup for one slash terms. 
 
+const markers = {
+   '/': 'one_slash',
+   '//': 'two_slash',
+   '{': 'one_brace',
+   '{{': 'two_brace',
 
+}
 
 function plugin(options) {
 
@@ -20,6 +25,11 @@ function plugin(options) {
 
    function termsTokenizer(eat, value, silent) {
       console.log(value)
+
+      // if this.escape doens't include the special term markers then add them so we can escape these characters. But will this work for doubles?
+
+      
+
 
       eat(value)
    }
--- harness.js
+++ harness.js
@@ -6,18 +6,22 @@ var remark2rehype = require('remark-rehype')
 var html = require('rehype-stringify')
 var report = require('vfile-reporter')
 
+
+var example = require('./sub_sup')
+
 const fs = require('fs')
 
 var processor = unified()
    .use(markdown)
-   .use(dev, {
-      test: "hello world"
-   })
+   // .use(dev, {
+   //    test: "hello world"
+   // })
+   .use(example)
    .use(remark2rehype)
    .use(html)
 
 
-processor.process(fs.readFileSync("readme.md"), function(err, file) {
+processor.process("# Title with ^**super**script^ with some text after.", function(err, file) {
    console.error(report(err || file))
-  // console.log(String(file))
+  console.log(String(file))
 })
